3487aefc14d999d3f1f20ee3802df4291f0f2a36
PrasiaLab <293825343+PrasiaLab@users.noreply.github.com>
2026-06-16 16:21:15 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
- /*
+/*
  * Firebase 공통 설정
  * 웹용 firebaseConfig는 공개되어도 되는 연결 정보입니다.
  */
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBTo1X0MbgYC-Kh_yqLrakXrBMoRibvwvk",
+export const firebaseConfig = {
+  apiKey: "Firebase 콘솔 구성 화면의 실제 API 키",
   authDomain: "prasialab.firebaseapp.com",
   projectId: "prasialab",
   storageBucket: "prasialab.firebasestorage.app",
@@ -12,8 +12,9 @@ const firebaseConfig = {
   appId: "1:844238283131:web:b46548f82cc9e5948d5dde",
   measurementId: "G-YSXY10S0XD"
 };
+
 /*
- * 최초 로그인 후 표시되는 관리자 UID를 아래에 입력하세요.
+ * 관리자 UID
  */
 export const ADMIN_UIDS = [
   "kgb0KhPYWLZYXt5YdAA7iuHMxYT2",
@@ -23,6 +24,5 @@ export const ADMIN_UIDS = [
 
 export function isConfiguredAdmin(uid) {
   return Boolean(uid) &&
-    ADMIN_UIDS.includes(uid) &&
-    uid !== "PASTE_ADMIN_UID_HERE";
+    ADMIN_UIDS.includes(uid);
 }
