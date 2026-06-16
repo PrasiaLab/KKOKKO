09de2e452989fa811b5f08b00234148f4da7d7ac
PrasiaLab <293825343+PrasiaLab@users.noreply.github.com>
2026-06-16 16:15:46 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -16,8 +16,8 @@ const firebaseConfig = {
  * 최초 로그인 후 표시되는 관리자 UID를 아래에 입력하세요.
  */
 export const ADMIN_UIDS = [
-  "kgb0KhPYWLZYXt5YdAA7iuHMxYT2"
-  "rkT0L6EK6Yd2IFNHBIQ8dVgqdHX2"
+  "kgb0KhPYWLZYXt5YdAA7iuHMxYT2",
+  "rkT0L6EK6Yd2IFNHBIQ8dVgqdHX2",
   "KhK0yllx62f20RU6YkUbxcDC3W53"
 ];
 
